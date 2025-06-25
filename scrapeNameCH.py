3858440e6e2844738581e60ca7b07ae435325ed7
asxvi <astanson02@gmail.com>
2025-06-25 06:10:44 -0500

    comments and debugging

--- scrapeNameCH.py
+++ scrapeNameCH.py
@@ -7,9 +7,15 @@ BASE_URL = 'https://catalog.uic.edu/' #parent website used for building url
 UIC_URL = 'https://catalog.uic.edu/all-course-descriptions/'
 
 
-# hours come out strange compared to the desired output, so we have to manually parse them
-# 3 possibilities: 'n' credits, 'n-m' credits, 'n or m' credits
-def parseHours(hours_str):
+def convHoursStrToRange(hours_str):
+    '''
+        Hours come out strange compared to the desired output, so we have to manually parse them
+        based on the 3 possibilities for credit hours: 'n' credits, 'n-m' credits, 'n or m' credits    
+        
+        PARAM: the string to parse/ reformat into a range 
+        RETURN: tuple of range: (smaller_num_possible_credits, larger_num_possible_credits)
+    '''
+
     # remove the words hour or hours and remove all extra white space
     clean_str = hours_str.lower().replace("hours", "").replace("hour", "").strip()
     first_num = None; second_num = None
@@ -17,7 +23,7 @@ def parseHours(hours_str):
     # first check for range, and take values to left and right of '-'
     if '-' in clean_str:
         first_num, second_num = clean_str.split('-')
-    # otherwise the credits are either 'n or m' creidts or 'n' credits
+    # otherwise the credits are either 'n or m' credits or 'n' credits
     elif 'or' in clean_str:
         items = clean_str.split('or')
         first_num = items[0].strip()
@@ -29,22 +35,28 @@ def parseHours(hours_str):
     return (first_num, second_num)
 
 
-def createListHours(hours_str):
+def convHourRangeToList(hours_str):
     '''
-        call helper parse function and return a string of the entire 
+        Call convHoursStrToRange helper function and return a string of the entire 
         range of credit hours for the course. 
+        
+        PARAM: the string to parse/ reformat into a range using convHoursStrToRange
+        RETURN: a string of all possible credit hours. Ex: "3,4,5...", or just '3' ...
         Note: Have to convert from int to str a few times within this entire process
     '''
 
-    first_num, second_num = parseHours(hours_str)
+    first_num, second_num = convHoursStrToRange(hours_str)
     rv = []
+    
+    # the range (first_num, second_num) should be 2 int values cast as strings. if not then we get ERROR
     try:
         first_num = int(first_num)
         second_num = int(second_num)
     except ValueError:
-        print(f"Can't parse the hours {hours_str}")
+        print(f"Can't parse the hours {hours_str}. Should be able to cast str as an int. Ex: int('5') == 5")
         rv = []
 
+    # either the CH range is 1 value ex: (3, 3) == '3', or multiple values ex: (1, 4) == '1,2,3,4'
     if first_num == second_num:
         rv = [first_num]
     else:
@@ -52,7 +64,11 @@ def createListHours(hours_str):
 
     return(','.join(map(str, rv)))
 
-def scrapeFrontPage():
+
+def scrapeFrontPage(URL):
+    '''
+
+    '''
     major_links_dict = {}
     
     response = requests.get(UIC_URL)
@@ -116,7 +132,7 @@ def getMasterCourseList(allCourses):
             
             # debugging
             # print(f"Code: {course_num}, Title: {course_title}, Hours: {course_hours}")
-            course_hours = (createListHours(course_hours))
+            course_hours = (convHourRangeToList(course_hours))
             
             subject = "".join(char for char in course_num if char.isalpha())
             # write to 2 different files. this should reduce the runtime by a little
@@ -170,7 +186,7 @@ def getCourseNames(URL_subject):
         print(f'BAD response: {response.status_code}')
 
 
-data = scrapeFrontPage()
+data = scrapeFrontPage(UIC_URL)
 
 # csLink = data['Computer Science (CS)']   #testing data point
 # scrapeSubject(csLink)
