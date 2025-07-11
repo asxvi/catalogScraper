4c574cd61d6aa5f862d48be4edd47dc829c13695
asxvi <astanson02@gmail.com>
2025-07-10 22:44:57 -0500

    time edge case non num CRN, ex: arranged time

--- scrapeTiming.py
+++ scrapeTiming.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-from typing
 import re
 from datetime import datetime
 
@@ -8,7 +7,7 @@ from datetime import datetime
 def getAllTimes(SUBJECT_URL: str) -> list:
     response = requests.get(SUBJECT_URL)
     if response.status_code == 200:
-        dayToTimeMap = {'M': 1, 'T': 2, 'W': 3, 'Th':4, 'F':5}
+        dayToTimeMap = {'M': 0, 'T': 24, 'W': 48, 'Th':72, 'F':96}
         returnString = ""
         
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -23,38 +22,53 @@ def getAllTimes(SUBJECT_URL: str) -> list:
             for row in rows[1:]:
                 vals = [td.text.strip() for td in row.find_all('td')]    
                 listOfTable.append(dict(zip(header, vals)))
+            
+            # print(currCourse)
+            # print(listOfTable)
+            # print()
+            # continue
 
             for row in listOfTable:
                 if 'Required' in row['CRN']:
                     continue
-                if 'lec' in row['Course Type'].lower():
+
+                print(row)
+                continue
+                if 'lec' in row['Course Type'].lower():                 # Prof wanted to start simple and later include lab/discus
                     print(row)
                     numTimes += 1
 
                     daysScheduled = row['Meeting Days']     # the days we meet
                     timeScheduled = row['Start & End Time'] # the time we meet
-                    addRem = 0
                     
+                    # garb = "11:00 AM - 12:15 PM"
                     # garb = "12:00 PM - 01:50 PM"
                     # start_raw, end_raw = [t.strip() for t in garb.split('-')]
-                    start_raw, end_raw = [t.strip() for t in timeScheduled.split('-')]
-                    intime = datetime.strptime(start_raw, "%I:%M %p").strftime("%H:%M")
-                    outtime = datetime.strptime(end_raw, "%I:%M %p").strftime("%H:%M")
 
-                    print(intime, outtime)
-                    # for i, t in enumerate(timeScheduled.split()):
-                    #     if 'AM' in t:    
-                    #         # print(timeScheduled.split()[i-1])
-                    #         intime = (datetime.strptime(timeScheduled.split()[i-1], "%I:%M %p").strftime("%H:%M"))
-                    #         outtime = 
-                    #         # print(i)
+                    start_raw, end_raw = [t.strip() for t in timeScheduled.split('-')]
+                    startTimeHM = datetime.strptime(start_raw, "%I:%M %p").strftime("%H:%M")
+                    endTimeHM = datetime.strptime(end_raw, "%I:%M %p").strftime("%H:%M")
+                    startTimeHour, startTimeMin = startTimeHM.split(':')[0], startTimeHM.split(':')[1]
+                    endTimeHour, endTimeMin = endTimeHM.split(':')[0], endTimeHM.split(':')[1]
 
                     day_pattern = re.compile(r"(Th|M|T|W|F)")
                     days = day_pattern.findall(daysScheduled)
-                    # for d in days:
-                        
 
-            break
+                    
+                    # days = ['Th', 'T']
+                    if 'Th' in days or 'T' in days:
+                        days.sort()
+                        
+                    # print(f'start: {startTimeHour, startTimeMin}')
+                    # print(f'end: {endTimeHour, endTimeMin}')
+
+                    startTime, endTime = 0, 0
+                    for d in days:
+                        startTime = dayToTimeMap[d] * 60 + int(startTimeHour) * 60 + int(startTimeMin)
+                        endTime = dayToTimeMap[d] * 60 + int(endTimeHour) * 60 + int(endTimeMin)
+                    
+
+            # break
 
 
         
