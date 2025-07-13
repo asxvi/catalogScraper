c288b88a3eabf9cc4d5837c8a87fa8be463ef597
asxvi <astanson02@gmail.com>
2025-07-12 21:21:44 -0500

    regex working on CHEM data

--- scrapePrereq.py
+++ scrapePrereq.py
@@ -42,32 +42,27 @@ def getPrerequisites(SUBJECT_URL):
         
         rv = ""
         for course in courses:            
-            
+            # get basic course info like the subject(CHEM), course_name(CHEM___121), course_description(paragraph)
             subject = course.find('strong').text.split()[0]
             course_name = course.find('strong').text
             course_name = (course_name.split()[0] + '___' + course_name.split()[1]).strip('.')
             course_description = course.find('p', class_='courseblockdesc').text
             
-
             prereqMatch = re.search("Prerequisite\s*(?:\(s\))?\s*:\s*([^\.]*)", course_description, re.IGNORECASE)
             if(prereqMatch):
                 prereqs = prereqMatch.group(1)
-
-                print(prereqs)
-                
                 clean_prereqs = prereqs.replace("\xa0", '___')
                 
-                prevTaken = re.findall("Grade\s*of\s*[A-F]\s*or\s*better\s*in\s+([A-Z]{2,4}___[0-9]{2,3})", clean_prereqs, re.IGNORECASE)
                 # concurrentTaken = re.findall("Credit\s*or\s*concurrent\s*registration\s*in\s+([A-Z]{2,4}___[0-9]{2,3})", clean_prereqs, re.IGNORECASE)
-                concurrentTaken = re.findall("\s*concurrent\s*\s+([A-Z]{2,4}___[0-9]{2,3})", clean_prereqs, re.IGNORECASE)
+                # concurrentTaken = re.findall("concurrent(?:\s+registration)?(?:\s+or)?(?:\s+[a-z]+)*\s+([A-Z]{2,4})___(\d{3})", clean_prereqs, re.IGNORECASE,)
+                prevTaken = re.findall("Grade\s*of\s*[A-F]\s*or\s*better\s*in\s+([A-Z]{2,4}___[0-9]{2,3})", clean_prereqs, re.IGNORECASE)
+                concurrentTaken = re.findall("concurrent(?:\s+registration)?(?:\s+or)?(?:\s+[a-z]+)*\s+([A-Z]{2,4}___\d{2,3})", clean_prereqs, re.IGNORECASE)
                 allNums = re.findall("([A-Z]{2,4}___[0-9]{2,3})", clean_prereqs, re.IGNORECASE)
-                
                 for c in allNums:
-                    if c in prevTaken:
-                        rv += (f'{c}\t{course_name}\t-1\n')
-                    elif c in concurrentTaken:
-                        print(f'{c}\t{course_name}\t0\n')
+                    if c in concurrentTaken:
                         rv += (f'{c}\t{course_name}\t0\n')
+                    elif c in prevTaken:
+                        rv += (f'{c}\t{course_name}\t-1\n')
                     else:
                         rv += (f'{c}\t{course_name}\t-1\n')
         
